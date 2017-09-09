5c6a081a585c1b0928e6b576bb2a0200e2a77816
Sid <sidsoft2014@outlook.com>
2017-09-09 23:29:39 +0000

    initial commit

---
+++ spec/scss-format-atom-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import ScssFormatAtomView from '../lib/scss-format-atom-view';
+
+describe('ScssFormatAtomView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
